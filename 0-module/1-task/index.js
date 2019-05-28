3329f6f586762a249005b01a2578639143970093
jsbasic-20190521_shaludkina-natalya-nikolaevna <nafanay2008@mail.ru>
2019-05-28 11:35:32 +0300

    First test task 0-module:
    Необходимо написать функцию, которая суммирует два числа
    
    sum(1,1) === 2
    sum(2,5) === 7

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,9 +1,18 @@
+"use strict"; //деректива для перевода код в режим полного соответствия современному стандарту
+
 /**
  * Power
  * @param {number} m base
  * @param {number} n index
  * @returns {number}
  */
-function sum (m, n) {
 
+function sum(m, n) {
+    return m + n;
 }
+
+var test = sum(1, 1);
+alert('1+1 = ' + test);
+
+var test2 = sum(2,5);
+alert('2+5 = ' + test2);
