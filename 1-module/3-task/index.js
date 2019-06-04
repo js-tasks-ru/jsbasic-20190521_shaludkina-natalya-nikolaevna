11b19db364422f180e9c839e0d737ee2f2ffc3b2
jsbasic-20190521_shaludkina-natalya-nikolaevna <nafanay2008@mail.ru>
2019-06-04 17:05:16 +0300

    3-task 1-module:
    Решение по задаче "Найти min/max в строке"

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,36 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+
+
+function getMinMax(inputData) {
+
+    //let inputData = '25,-1,-234,4,   1000';
+
+    let arr = inputData.split(/[\s,]+/);
+
+    let arrOfNumber = arr.map(string => parseFloat(string));
+    alert(arrOfNumber);
+
+
+    let arrOfOnlyNumber = arrOfNumber.filter(function(num) {
+        if (isNumeric(num)) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    alert(arrOfOnlyNumber);
+
+
+    function isNumeric(num) {
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    }
+
+
+    let min = Math.min.apply(null, arrOfOnlyNumber);
+    let max = Math.max.apply(null, arrOfOnlyNumber);
+
+    return { min , max };
 
 }
-
